c6067f19b5ba471cdcdf28bb2cde84ad5f254992
qiaoli wang <414217795zoe@gmail.com>
2020-11-17 18:05:22 +1100

    自提点管理页面

--- src/pages/pickUpSite/index.jsx
+++ src/pages/pickUpSite/index.jsx
@@ -1,10 +1,11 @@
 import Taro from '@tarojs/taro'
 import React, { Component } from 'react'
 import { View, Text } from '@tarojs/components'
-import { AtButton,AtSearchBar } from 'taro-ui'
+import { AtButton,AtSearchBar,AtIcon } from 'taro-ui'
 
 import "taro-ui/dist/style/components/button.scss" // 按需引入
 import "taro-ui/dist/style/components/search-bar.scss";
+import "taro-ui/dist/style/components/icon.scss";
 import './index.less'
 
 export default class Index extends Component {
@@ -51,17 +52,28 @@ export default class Index extends Component {
                      onActionClick={this.onSearchActionClick.bind(this)}
         />
         <View className='pick-up-list'>
-          <View className='site-item'>
+          <View className='site-item bg-white'>
              <View className='site-name'>Le Crois</View>
              <View className='address'>
                235 Springvale Road, Glen Waverley, Victoria, Australia
              </View>
             <View className='item-bottom'>
-              <View className='green'>已设置定位</View>
-              <View>取消定位</View>
-              <View>定位</View>
-              <View>编辑</View>
-              <View>删除</View>
+              <View className='item-flex green'><AtIcon className='mr-10' value='check-circle' size='16' color='#2EB820'></AtIcon> 已设置定位</View>
+              <View className='item-flex'><AtIcon className='mr-10' value='close-circle' size='16' color='#666'></AtIcon>取消定位</View>
+              <View className='item-flex'><AtIcon className='mr-10' value='iphone' size='16' color='#666'></AtIcon> 编辑</View>
+              <View className='item-flex'><AtIcon className='mr-10' value='trash' size='16' color='#666'></AtIcon> 删除</View>
+            </View>
+          </View>
+          <View className='site-item bg-white'>
+            <View className='site-name'>Le Crois</View>
+            <View className='address'>
+              235 Springvale Road, Glen Waverley, Victoria, Australia
+            </View>
+            <View className='item-bottom'>
+              <View className='item-flex red'><AtIcon className='mr-10' value='close-circle' size='16' color='#FF3A29'></AtIcon> 未设置定位</View>
+              <View className='item-flex'><AtIcon className='mr-10' value='map-pin' size='16' color='#666'></AtIcon> 定位</View>
+              <View className='item-flex'><AtIcon className='mr-10' value='iphone' size='16' color='#666'></AtIcon> 编辑</View>
+              <View className='item-flex'><AtIcon className='mr-10' value='trash' size='16' color='#666'></AtIcon> 删除</View>
             </View>
           </View>
           <View className='no-data'>
